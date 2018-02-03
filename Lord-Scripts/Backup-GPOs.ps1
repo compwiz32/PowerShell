dd08313d9679dac596f890d2d45391ef6af1325f
Mike Kanakos <mkanakos@gmail.com>
2018-02-02 19:24:07 -0500

    folder changes

---
+++ Lord-Scripts/Backup-GPOs.ps1
@@ -0,0 +1,17 @@
+﻿# MK Script created 2017-04-10
+
+# Script created from GPOBackupSamp.PS1 script found online at:
+# https://gallery.technet.microsoft.com/scriptcenter/Backup-All-GPOs-Powershell-bcdb7b5e#content
+# This script will Backup all GPOs and save it to a network folder named as the current date.
+
+# Load GroupPolicy module
+Import-Module grouppolicy
+
+# Set the date format
+$date = get-date -format yyyy.MM.dd
+
+# Create new directory
+New-Item -Path \\lord\americas\IT\Infrastructure\Backups\Group-Policy\$date -ItemType directory
+
+# Backup GPO's to new directory
+Backup-Gpo -All -Path \\lord\americas\IT\Infrastructure\Backups\Group-Policy\$date
