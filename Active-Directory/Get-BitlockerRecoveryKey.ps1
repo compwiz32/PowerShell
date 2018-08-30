6ba1d7fc2407b90a1249d8b55370603fd8b655cb
Mike Kanakos <mkanakos@gmail.com>
2018-08-30 17:28:56 -0400

    August Sync

--- Active-Directory/Get-BitlockerRecoveryKey.ps1
+++ Active-Directory/Get-BitlockerRecoveryKey.ps1
@@ -2,22 +2,23 @@ Function Get-BitlockerRecoveryKey
 {
 <#
 .Synopsis 
-  Gets Bitlocker Recovery info stored in Active Directory.
+ Gets Bitlocker Recovery info stored in Active Directory.
   
-  .Description 
-  This function retrieves the Bitlocker Recovery information stored in Active Directory.
-  You must be a Domain Administrator or have the permissions delegated to your account in 
-  order to retrieve the information from A/D.
-  *** BitLocker info will only exist if a PC is encrypted with BitLocker ***
+.Description 
+ This function retrieves the Bitlocker Recovery information stored in Active Directory.
+ You must be a Domain Administrator or have the permissions delegated to your account in 
+ order to retrieve the information from A/D.
 
-  .Example 
-  Get-BitLockerRecoveryKey -computername  CRDNAB-183TXW1
-  
-  DistinguishedName      : CN=2015-12-10T12:36:09-05:00{5B99B6BF-2DFF-4F3C-B865-33FF1F370E91},CN=CRDNAB-183TXW1,OU=Computers,OU=XXX,OU=XXX,DC=XXX,DC=LOCAL
-  msFVE-RecoveryPassword : 308759-XXXXXX-000000-XXXXXX-000000-XXXXXX-000000-621445
-  Name                   : 2015-12-10T12:36:09-05:00{5B99B6BF-2DFF-4F3C-B865-33FF1F370E91}
-  ObjectClass            : msFVE-RecoveryInformation
-  ObjectGUID             : 01a93433-cbcb-48e2-bb4b-74f20d1fef6c 
+ *** BitLocker info will only exist if a PC is encrypted with BitLocker ***
+
+.Example 
+ Get-BitLockerRecoveryKey -computername  CRDNAB-183TXW1
+
+ DistinguishedName      : CN=2015-12-10T12:36:09-05:00{5B99B6BF-2DFF-4F3C-B865-33FF1F370E91},CN=CRDNAB-183TXW1,OU=Computers,OU=XXX,OU=XXX,DC=XXX,DC=LOCAL
+ msFVE-RecoveryPassword : 308759-XXXXXX-000000-XXXXXX-000000-XXXXXX-000000-621445
+ Name                   : 2015-12-10T12:36:09-05:00{5B99B6BF-2DFF-4F3C-B865-33FF1F370E91}
+ ObjectClass            : msFVE-RecoveryInformation
+ ObjectGUID             : 01a93433-cbcb-48e2-bb4b-74f20d1fef6c 
 #>
 
 
---
+++ Inventory-Tools/Get-PageFileInfo.ps1
@@ -0,0 +1,114 @@
+Function Get-PageFileInfo 
+{
+
+<# 
+ .Synopsis 
+  Returns info about the page file size of a Windows computer. Defaults to local machine. 
+
+ .Description 
+  Returns the pagefile size info in MB. Also returns the PageFilePath, PageFileTotalSize, PagefileCurrentUsage,
+  and PageFilePeakusage. Also returns if computer is using a TempPafeFile and if the machine's pagefile is
+  managed by O/S (AutoManaged = true) or statically set (AutoManaged = False)
+
+  
+
+ .Example 
+  Get-PageFileInfo -computername SRV01
+  Returns pagefile info for the computer named SRV01
+
+  Computer             : SRV01
+  FilePath             : C:\pagefile.sys
+  AutoManagedPageFile  : True
+  TotalSize (in MB)    : 8192
+  CurrentUsage (in MB) : 60
+  PeakUsage (in MB)    : 203
+  TempPageFileInUse    : False
+
+
+ .Example 
+  Get-PageFileInfo SRV01, SRV02
+  Returns pagefile info for two computers named SRV01 & DC02.
+
+  Computer             : SRV01
+  FilePath             : C:\pagefile.sys
+  AutoManagedPageFile  : True
+  TotalSize (in MB)    : 8192
+  CurrentUsage (in MB) : 60
+  PeakUsage (in MB)    : 203
+  TempPageFileInUse    : False
+
+  Computer             : SRV02
+  FilePath             : C:\pagefile.sys
+  AutoManagedPageFile  : True
+  TotalSize (in MB)    : 8192
+  CurrentUsage (in MB) : 0
+  PeakUsage (in MB)    : 0
+  TempPageFileInUse    : False
+
+.Example 
+  Get-PageFileInfo SRV01, SRV02, SRV03 | Format-Table
+  Returns pagefile info for three computers named SRV01, SRV02 & SRV03 in a table format.
+
+
+  Computer  FilePath        AutoManagedPageFile TotalSize (in MB) CurrentUsage (in MB) PeakUsage (in MB) TempPageFileInUse
+  --------  --------        ------------------- ----------------- -------------------- ----------------- -----------------
+  SRV01    C:\pagefile.sys                True              8192                   60               203             False
+  SRV02    C:\pagefile.sys                True             13312                    0                 0             False
+  SRV03    C:\pagefile.sys                True              2432                    0                 0             False
+
+ 
+  .Parameter computername 
+  The name of the computer to query. Required field.
+
+ .Notes 
+  NAME: Get-PageFileInfo 
+  AUTHOR: Mike Kanakos 
+  Version: v1.1
+  LASTEDIT: Thursday, August 30, 2018 2:19:18 PM
+  
+  .Link 
+  
+  
+#> 
+
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True,ValueFromPipeline=$True)]  
+    [string[]]$ComputerName
+)
+
+# Main Part of function
+
+
+Foreach ($computer in $ComputerName)
+{
+
+  $online= Test-Connection -ComputerName $computer -Count 2 -Quiet
+    if ($online -eq $true)
+     {
+      $PageFileResults = Get-CimInstance -Class Win32_PageFileUsage -ComputerName $Computer | Select-Object *
+      $CompSysResults = Get-CimInstance win32_computersystem  -Namespace 'root\cimv2'
+    
+      $PageFileStats = [PSCustomObject]@{
+        Computer = $computer
+        FilePath = $PageFileResults.Description
+        AutoManagedPageFile = $CompSysResults.AutomaticManagedPagefile
+        "TotalSize(in MB)" = $PageFileResults.AllocatedBaseSize
+        "CurrentUsage(in MB)"  = $PageFileResults.CurrentUsage
+        "PeakUsage(in MB)" = $PageFileResults.PeakUsage
+        TempPageFileInUse = $PageFileResults.TempPageFile
+      } #END PSCUSTOMOBJECT
+     } #END IF
+    else
+     {
+        # Computer is not reachable!
+        Write-Host "Error: $computer not online" -Foreground white -BackgroundColor Red
+     } # END ELSE
+
+
+  $PageFileStats
+ 
+} #END FOREACH
+
+
+} #END FUNCTION
