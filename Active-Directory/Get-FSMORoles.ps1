d7fa3e0f2904cb70181a12efaa2d6ee34f2b1193
Mike Kanakos <mkanakos@gmail.com>
2019-01-15 21:06:15 -0500

    Rename display objects

--- Active-Directory/Get-FSMORoles.ps1
+++ Active-Directory/Get-FSMORoles.ps1
@@ -1,5 +1,6 @@
 ﻿# Get-FSMO-Roles
 # Mike Kanakos - 2016-02-04
+# found code online and cobbled together
 
 # Assign .net connections to variables
 $SchemaMaster = ([System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()).SchemaRoleOwner
@@ -10,43 +11,43 @@ $RIDMaster = ([System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomai
 
 
 # Create an array to store SchemaMaster values
-$DisplayObj = New-Object psobject
-$DisplayObj | Add-Member -type NoteProperty -Name 'Role' -Value 'Schema Master'
-$DisplayObj | Add-Member -type NoteProperty -Name 'Name' -Value $SchemaMaster.Name 
-$DisplayObj | Add-Member -type NoteProperty -Name 'IP Address' -Value $SchemaMaster.IPAddress
-$DisplayObj | Add-Member -type NoteProperty -Name 'Site' -Value $SchemaMaster.SiteName 
+$DisplayObjSchema = New-Object psobject
+$DisplayObjSchema | Add-Member -type NoteProperty -Name 'Role' -Value 'Schema Master'
+$DisplayObjSchema | Add-Member -type NoteProperty -Name 'Name' -Value $SchemaMaster.Name
+$DisplayObjSchema | Add-Member -type NoteProperty -Name 'IP Address' -Value $SchemaMaster.IPAddress
+$DisplayObjSchema | Add-Member -type NoteProperty -Name 'Site' -Value $SchemaMaster.SiteName
 
 # Create an array to store PDC Emulator values
-$DisplayObj2 = New-Object psobject
-$DisplayObj2 | Add-Member -type NoteProperty -Name 'Role' -Value 'PDC Emulator'
-$DisplayObj2 | Add-Member -type NoteProperty -Name 'Name' -Value $PDCEmulator.Name 
-$DisplayObj2 | Add-Member -type NoteProperty -Name 'IP Address' -Value $PDCEmulator.IPAddress
-$DisplayObj2 | Add-Member -type NoteProperty -Name 'Site' -Value $PDCEmulator.SiteName 
+$DisplayObjPDC = New-Object psobject
+$DisplayObjPDC | Add-Member -type NoteProperty -Name 'Role' -Value 'PDC Emulator'
+$DisplayObjPDC | Add-Member -type NoteProperty -Name 'Name' -Value $PDCEmulator.Name
+$DisplayObjPDC | Add-Member -type NoteProperty -Name 'IP Address' -Value $PDCEmulator.IPAddress
+$DisplayObjPDC | Add-Member -type NoteProperty -Name 'Site' -Value $PDCEmulator.SiteName
 
 # Create an array to store Domain Naming Master values
-$DisplayObj3 = New-Object psobject
-$DisplayObj3 | Add-Member -type NoteProperty -Name 'Role' -Value 'Domain Naming Master'
-$DisplayObj3 | Add-Member -type NoteProperty -Name 'Name' -Value $DomainNamingMaster.Name 
-$DisplayObj3 | Add-Member -type NoteProperty -Name 'IP Address' -Value $DomainNamingMaster.IPAddress
-$DisplayObj3 | Add-Member -type NoteProperty -Name 'Site' -Value $DomainNamingMaster.SiteName
+$DisplayObjDomNaming = New-Object psobject
+$DisplayObjDomNaming | Add-Member -type NoteProperty -Name 'Role' -Value 'Domain Naming Master'
+$DisplayObjDomNaming | Add-Member -type NoteProperty -Name 'Name' -Value $DomainNamingMaster.Name
+$DisplayObjDomNaming | Add-Member -type NoteProperty -Name 'IP Address' -Value $DomainNamingMaster.IPAddress
+$DisplayObjDomNaming | Add-Member -type NoteProperty -Name 'Site' -Value $DomainNamingMaster.SiteName
 
 # Create an array to store Infrastructure Master values
-$DisplayObj4 = New-Object psobject
-$DisplayObj4 | Add-Member -type NoteProperty -Name 'Role' -Value 'Infrastructure Master'
-$DisplayObj4 | Add-Member -type NoteProperty -Name 'Name' -Value $InfraMaster.Name 
-$DisplayObj4 | Add-Member -type NoteProperty -Name 'IP Address' -Value $InfraMaster.IPAddress
-$DisplayObj4 | Add-Member -type NoteProperty -Name 'Site' -Value $InfraMaster.SiteName
+$DisplayObjINFRA = New-Object psobject
+$DisplayObjINFRA | Add-Member -type NoteProperty -Name 'Role' -Value 'Infrastructure Master'
+$DisplayObjINFRA | Add-Member -type NoteProperty -Name 'Name' -Value $InfraMaster.Name
+$DisplayObjINFRA | Add-Member -type NoteProperty -Name 'IP Address' -Value $InfraMaster.IPAddress
+$DisplayObjINFRA | Add-Member -type NoteProperty -Name 'Site' -Value $InfraMaster.SiteName
 
 # Create an array to store RID Master values
-$DisplayObj5 = New-Object psobject
-$DisplayObj5 | Add-Member -type NoteProperty -Name 'Role' -Value 'RID Master'
-$DisplayObj5 | Add-Member -type NoteProperty -Name 'Name' -Value $RIDMaster.Name 
-$DisplayObj5 | Add-Member -type NoteProperty -Name 'IP Address' -Value $RIDMaster.IPAddress
-$DisplayObj5 | Add-Member -type NoteProperty -Name 'Site' -Value $RIDMaster.SiteName
+$DisplayObjRID = New-Object psobject
+$DisplayObjRID | Add-Member -type NoteProperty -Name 'Role' -Value 'RID Master'
+$DisplayObjRID | Add-Member -type NoteProperty -Name 'Name' -Value $RIDMaster.Name
+$DisplayObjRID | Add-Member -type NoteProperty -Name 'IP Address' -Value $RIDMaster.IPAddress
+$DisplayObjRID | Add-Member -type NoteProperty -Name 'Site' -Value $RIDMaster.SiteName
 
 # Create another array to store the all the previous arrays in one large array
 $Array = @()
-$Array += $DisplayObj, $DisplayObj2, $DisplayObj3, $DisplayObj4, $DisplayObj5
+$Array += $DisplayObjSchema, DisplayObjPDC, $DisplayObjDomNaming, $DisplayObjINFRA, $DisplayObjRID
 
 
 #Display results
