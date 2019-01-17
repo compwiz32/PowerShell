564cba011349ee227ad3c38c81d765cdebba0ec7
Mike Kanakos <mkanakos@gmail.com>
2019-01-17 14:32:11 -0500

    fixed where-object syntax

--- Active-Directory/Get-ServersMissingManagedBy.ps1
+++ Active-Directory/Get-ServersMissingManagedBy.ps1
@@ -27,9 +27,9 @@ function Get-ServersMissingManagedBy {
 
     #>
 
-    [CmdletBinding()]
 
     Get-adcomputer -filter 'OperatingSystem -like "*server*" '  -prop operatingsystem, description, managedby, memberof |
-    Where-Object {($_.enabled -eq $true) -and ($_.managedby -eq $null) }
+    Where-Object { ($_.enabled -eq $true) -and ($_.description -notlike '*Failover cluster*') -and `
+    ($_.description -notlike '*template*') -and ($_.description -notlike '*inactive*') -and ($_.managedby -eq $null) }
 
 } #End of Function
