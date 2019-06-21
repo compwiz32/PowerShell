54d1d797fa73509e33280479a410746aa4c96b9c
Mike Kanakos <mkanakos@gmail.com>
2019-06-21 14:00:59 -0400

    add catch blocks

--- !InProgress/Set-MS15_124VulnRegKeys.ps1
+++ !InProgress/Set-MS15_124VulnRegKeys.ps1
@@ -3,20 +3,33 @@ $Path64 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Internet Explorer\MAIN\FeatureCo
 $Path32 = "HKLM:\SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl"
 
 invoke-command -computername $nodelist -scriptblock {
-               
+                
+    
     Try {
         #32 bit internet explorer regpath
-        New-Item -Path $using:Path32 -Name FEATURE_ALLOW_USER32_EXCEPTION_HANDLER_HARDENING
+        New-Item -Path $using:Path32 -Name FEATURE_ALLOW_USER32_EXCEPTION_HANDLER_HARDENING -ErrorAction Stop
         New-ItemProperty -Path $using:Path32\FEATURE_ALLOW_USER32_EXCEPTION_HANDLER_HARDENING -Name "iexplore.exe" -PropertyType DWORD -Value 1
+    }
 
-        #64 bit inter explorer regpath
-        New-Item -Path $using:Path64 -Name FEATURE_ALLOW_USER32_EXCEPTION_HANDLER_HARDENING
-        New-ItemProperty -Path $using:Path64\FEATURE_ALLOW_USER32_EXCEPTION_HANDLER_HARDENING -Name "iexplore.exe" -PropertyType DWORD -Value 1
-
-    }#end Try
-    
     Catch {
-                            
-    } #end Catch
-        
-} #end Scriptblock
+        Write-Warning "Unable to create item at $using:Path32"
+    }
+
+    Try {   
+        #64 bit inter explorer regpath
+        New-Item -Path $using:Path64 -Name FEATURE_ALLOW_USER32_EXCEPTION_HANDLER_HARDENING -ErrorAction Stop
+        New-ItemProperty -Path $using:Path64\FEATURE_ALLOW_USER32_EXCEPTION_HANDLER_HARDENING -Name "iexplore.exe" -PropertyType DWORD -Value 1
+    
+    }
+
+    Catch {
+        Write-Warning "Unable to create item at $using:Path64"
+    }
+ 
+} -ErrorAction Stop #end Scriptblock
+
+Catch {
+
+Write-Warning "Unable to make connection to $psitem"
+
+} #catch
