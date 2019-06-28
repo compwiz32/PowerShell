3386ff4909a476d908ba27a3cb11678d14f3535f
Mike Kanakos <mkanakos@gmail.com>
2019-06-28 09:56:58 -0400

    move script to permanent repo location

--- Inventory-Tools/Set-MS15_124VulnRegKeys.ps1
+++ Inventory-Tools/Set-MS15_124VulnRegKeys.ps1
@@ -1,4 +1,4 @@
-$nodelist = Get-Content C:\Scripts\Input\MS15-124_Hosts.txt
+$nodelist = Get-Content C:\Scripts\Input\MS15-124_HostsB.txt
 $Path64 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Internet Explorer\MAIN\FeatureControl"
 $Path32 = "HKLM:\SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl"
 
