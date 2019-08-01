9315682ce429d1d5e480bffd127be43e7a28e6be
Mike Kanakos <mkanakos@gmail.com>
2019-07-31 21:53:42 -0400

    replace wonky quotes

--- Set-LanmanServerSharesFilePath.ps1
+++ Set-LanmanServerSharesFilePath.ps1
@@ -2,78 +2,62 @@ Function Set-LanmanServerSharesFilePath {
 
     <#
     .Synopsis
-        Returns a customized list of Active Directory account information for a single user
+        Sets (or changes) the file path for Windows shares in the registry for the LanmanServerSharesFilePath reg key
 
     .Description
-        Returns a customized list of Active Directory account information for a single user.
-        The customized list is a combination of the fields that are most commonly needed to review
-        when an employee calls the helpdesk for assistance.
+        Sets (or changes) the file path for Windows shares in the registry for the LanmanServerSharesFilePath reg key.
+        Use this function to update file paths in bulk for Windows shares.
 
     .Example
-        Get-EmployeeInfo Tom_Brady
-        Returns a customized list of AD account information for user account named Tom_Brady
+        Set-LanmanServerSharesFilePath -OldPath c:\ -NewPath d:\
+        
+        Finds shares in Set-LanmanServerSharesFilePath reg key that match the old path and updates the value with the 
+        new path
 
-            PS C:\Scripts> Get-EmployeeInfo Michael_Kanakos
+    .Parameter OldFilePath
+        Existing file path in regkey named Set-LanmanServerSharesFilePath
 
-            FirstName    : Tom
-            LastName     : Brady
-            Title        : Server Engineer
-            Department   : Strategic Server Solutions
-            Manager      : Bill_Belichick
-            City         : Cary
-            EmployeeID   : 123456
-            UserName     : Tom_Brady
-            DisplayNme   : Brady, Tom
-            EmailAddress : Tom_Brady@NWTraders.msft
-            OfficePhone  : +1 919-555-1212
-            MobilePhone  : +1 631-555-1212
+    .Parameter OldFilePath
+        New file path that will be updated in Set-LanmanServerSharesFilePath
 
-            PasswordExpired       : False
-            AccountLockedOut      : False
-            LockOutTime           : 0
-            AccountEnabled        : True
-            AccountExpirationDate :
-            PasswordLastSet       : 3/26/2018 9:29:02 AM
-            PasswordExpireDate    : 9/28/2018 9:29:02 AM
-
-    .Parameter UserName
-        The employee account to lookup in Active Directory
 
     .Notes
-        NAME:        Add-EmployeeInfo.ps1
+        NAME:        Set-LanmanServerSharesFilePath.ps1
         AUTHOR:      Mike Kanakos
-        VERSION:     1.0.3
-        DateCreated: 2018-04-10
-        DateUpdated: 2019-01-15
+        VERSION:     1.0.0
+        DateCreated: 2019-07-31
+        DateUpdated: 2019-07-31
 
-        v 1.0.3 - update help file formatting
+        v 1.0.0 - initial function build
 
     .Link
         https://github.com/compwiz32/PowerShell
 
+        Originally found at https://martin77s.wordpress.com/2014/12/21/changing-shared-folders-path/
+
 #>
-
-
-
 
     [cmdletbinding(SupportsShouldProcess = $true)]
     param(
-        $OldFilePath,
-        $NewFilePath
+        [Parameter(Mandatory = $True, Position = 0)]
+        [String]$OldFilePath,
+        
+        [Parameter(Mandatory = $True, Position = 1)]
+        [String]$NewFilePath
     )
  
-    $RegPath = ‘HKLM:SYSTEMCurrentControlSetServicesLanmanServerShares’
+    $RegPath = 'HKLM:SYSTEM\CurrentControlSet\Services\LanmanServer\Shares'
 
     dir -Path $RegPath | Select-Object -ExpandProperty Property | ForEach-Object {
         $ShareName = $_
         $ShareData = Get-ItemProperty -Path $RegPath -Name $ShareName |
         Select-Object -ExpandProperty $ShareName
-        if ($ShareData | Where-Object { $_ -eq “Path=$OldPath“ }) {
-            $ShareData = $ShareData -replace [regex]::Escape(“Path=$OldPath“), “Path=$NewPath“
+        if ($ShareData | Where-Object { $_ -eq "Path=$OldFilePath" }) {
+            $ShareData = $ShareData -replace [regex]::Escape("Path=$OldFilePath"), "Path=$NewFilePath"
  
-            if ($PSCmdlet.ShouldProcess($ShareName, ‘Change-SharePath’)) {
+            if ($PSCmdlet.ShouldProcess($ShareName, 'Change-SharePath')) {
                 Set-ItemProperty -Path $RegPath -Name $ShareName -Value $ShareData
-            } #nd if Set-ItemProperty
+            } #end if Set-ItemProperty
         } #end If $sharedata
     } #end ForEach
 } #end Function
