09b7531b6380c300168f020dfb72594b823b024d
Mike Kanakos <mkanakos@gmail.com>
2019-09-17 17:59:31 -0400

    fix broken code, add examples, add parameter, add foreach

--- Active-Directory/Get-ServerOwner.ps1
+++ Active-Directory/Get-ServerOwner.ps1
@@ -1,79 +1,96 @@
 function Get-ServerOwner {
 
-<#
-.SYNOPSIS
-    Returns a list of servers and the associated owner assigned in Active Directory
+    <#
+    .SYNOPSIS
+        Returns the associated owner assigned for the specified server(s). Also included are the operating system name
+        and the location where the object resides in Active Directory
 
-.DESCRIPTION
-    Returns a list of servers and the associated owner assigned in Active Directory. The list of servers is determined
-    by querying the operatingsystem field and searching for *server*. Also, disabled computer objects are filtered out
-    of the view.
+    .DESCRIPTION
+        Returns the associated owner assigned for the specified server(s). Also included are the operating system name
+        and the location where the object resides in Active Directory
+
+    .PARAMETER ComputerName
+        The name of the computer you want to query. More than one computer can be entered by seperating each computername
+        should be with a comma. 
+
+    .EXAMPLE
+        Get-ServerOwner SRV01
+
+        Name        ManagedBy           OperatingSystem                   CanonicalName
+        ----        ---------           ---------------                   -------------
+        SRV01       Michael_Kanakos     Windows Server 2012 R2 Standard   NWTraders.MSFT/Computers/SRV01
+
+    .EXAMPLE
+        Get-ServerOwner SRV01 | Format-List
+
+        Name            : SRV01
+        ManagedBy       : Michael_Kanakos
+        OperatingSystem : Windows Server 2012 R2 Standard
+        CanonicalName   : NWTraders.MSFT/Computers/SRV01
+
+        
+    .EXAMPLE
+        Get-ServerOwner Svr01, svr02, svr03 | Format-Table
+
+        Name     ManagedBy           OperatingSystem                      CanonicalName
+        ----     ---------           ---------------                      -------
+        SRV01    Michael_Kanakos     Windows Server 2012 R2 Standard      NWTraders.MSFT/Computers/SRV01
+        SRV02    Derek_Jeter         Windows Server 2012 R2 Standard      NWTraders.MSFT/Computers/SRV02
+        SRV03    Tom_Brady           Windows Server 2012 R2 Standard      NWTraders.MSFT/Computers/SRV03
 
 
-.EXAMPLE
-    Get-ServerOwner
+    .NOTES
+        Name: Get-ServerOwner
+        Author: Mike Kanakos
+        Version: 1.0.0
+        DateCreated: 2019-06-05
+        DateUpdated: 2019-09-17
 
-    Name                 ManagedBy           OperatingSystem                        Enabled
-    ----                 ---------           ---------------                        -------
-    SRV01                Michael_Kanakos     Windows Server 2012 R2 Standard        True
-    SRV02                Derek_Jeter         Windows Server 2012 R2 Standard        True
-    SRV01                Tom_Brady           Windows Server 2012 R2 Standard        True
+        v1.0.1 - fix broken code, add examples, add parameter, add foreach
 
-
-.NOTES
-    Name: Get-ServerOwner
-    Author: Mike Kanakos
-    Version: 1.0.0
-    DateCreated: 2019-06-05
-    DateUpdated: 2019-06-05
-
-    v1.0.0 - initial script build
-
-.LINK
-    https://www.github.com/compwiz32/Powershell
+    .LINK
+        https://www.github.com/compwiz32/Powershell
 
 #>
 
     [CmdletBinding()]
-    param(
-
+    Param(
+        [alias('DnsHostName', '__SERVER', 'Computer', 'IPAddress')]
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true)]
+        [string[]]$ComputerName
     )
+
 
     begin {
 
-    }
+    } #end of begin
 
     process {
-        $ADUserParams = @{
-            Filter = {operatingsystem -like '*server*'}
-            Properties =    'OperatingSystem',
-                            'ManagedBy',
-                            'CanonicalName',
-                            'Enabled'
-            }
+        
+        foreach ($Node in $ComputerName) {
+            
+            $ParamHash = @{
+                Identity   = $Node
+                Properties = 'OperatingSystem',
+                'ManagedBy',
+                'CanonicalName',
+                'Enabled'
+            } #end ADUserParams
 
-        $SelectParams = @{
-            Property =      'Name',
-                            @{Name='ManagedBy';Expression={(Get-ADUser ($_.managedBy)).samaccountname}},
-                            'OperatingSystem',
-                            'Enabled'
-            }
+            $SelectParams = @{
+                Property = 'Name',
+                @{Name = 'ManagedBy'; Expression = { (Get-ADUser ($_.managedBy)).samaccountname } },
+                'OperatingSystem',
+                'CanonicalName'
+            } #end SelectParams
 
+            Get-ADComputer @ParamHash | Where-Object { $_.enabled -eq $true } | Select-Object @SelectParams
+        
+        } #end ForEach
 
-        get-adcomputer @ADUserParams  | Where-Object {$_.enabled -eq $true} | Select-Object @SelectParams
-    }
+    } #end Process block
 
     end {
-    }
-}
+    } #end End block
 
-
-
-
-
-
-
-
-<# get-adcomputer -filter "operatingsystem -like '*server*'" -prop operatingsystem, managedby, enabled | Where-Object {$_.enabled
-    -eq $true} |  select name, @{Name='ManagedBy';Expression={(Get-ADUser ($_.managedBy)).samaccountname}}, operatingsystem, enabled
-    #>
+} #end of Function
