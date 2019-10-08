117cdbcf596313a7946c5c6c89e20f8afbf01b52
Mike Kanakos <mkanakos@gmail.com>
2019-10-08 16:24:11 -0400

    build out params

--- !InProgress/Set-PSConsoleConfiguration.ps1
+++ !InProgress/Set-PSConsoleConfiguration.ps1
@@ -8,17 +8,9 @@ function Set-PSConsoleConfiguration {
         Sets the window size, font and font attributes for the PowerShell console window. Allows for programatically
         setting the properties of the PowerShell console without having to interact with the GUI.
 
-    .PARAMETER ForegroundColor
-        The name of the computer you want to query. More than one computer can be entered by seperating each computername
-        should be with a comma.
-
-    .PARAMETER BackgroundColor
-        The name of the computer you want to query. More than one computer can be entered by seperating each computername
-        should be with a comma.
-
     .PARAMETER BufferSize
-    The name of the computer you want to query. More than one computer can be entered by seperating each computername
-    should be with a comma.
+        The name of the computer you want to query. More than one computer can be entered by seperating each computername
+        should be with a comma.
 
     .PARAMETER WindowWidth
         The name of the computer you want to query. More than one computer can be entered by seperating each computername
@@ -29,6 +21,14 @@ function Set-PSConsoleConfiguration {
         should be with a comma.
 
     .PARAMETER WindowTitle
+        The name of the computer you want to query. More than one computer can be entered by seperating each computername
+        should be with a comma.
+
+    .PARAMETER ForegroundColor
+        The name of the computer you want to query. More than one computer can be entered by seperating each computername
+        should be with a comma.
+
+    .PARAMETER BackgroundColor
         The name of the computer you want to query. More than one computer can be entered by seperating each computername
         should be with a comma.
 
@@ -66,24 +66,46 @@ function Set-PSConsoleConfiguration {
 
 
     .NOTES
-        Name: Get-ServerOwner
+        Name: Set-PSConsoleConfiguration
         Author: Mike Kanakos
         Version: 1.0.0
-        DateCreated: 2019-06-05
-        DateUpdated: 2019-09-17
+        DateCreated: 2019-10-08
+        DateUpdated: 2019-10-08
 
-        v1.0.1 - fix broken code, add examples, add parameter, add foreach
+        v1.0.0 - initial script build
 
     .LINK
-        https://www.github.com/compwiz32/Powershell
+        https://www.github.com/compwiz32/Powershell/Set-PSConsoleConfiguration.md
 
     #>
 
 
     [CmdletBinding()]
     param (
+        # screen buffer size
+        [Parameter()]
+        [ValidateRange(1..9999)]
+        [int]
+        $BufferSize
 
-    )
+        # console window width
+        [Parameter()]
+        [ValidateRange(20..300)]
+        [int]
+        $WindowWidth
+
+        # console window height
+        [Parameter()]
+        [ValidateRange(20-60)]
+        [int]
+        $WindowHeight
+
+        # console title text
+        [Parameter()]
+        [string]
+        $MessageTitle
+        )
+
 
     begin {
 
