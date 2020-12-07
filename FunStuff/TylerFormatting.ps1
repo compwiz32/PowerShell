570380b9382887ae1624021e972bc68b0386c368
compwiz32 <mkanakos@gmail.com>
2020-12-06 19:24:37 -0500

    files for teaching PS to Tyler

---
+++ FunStuff/TylerFormatting.ps1
@@ -0,0 +1,17 @@
+$first = "Tyler "
+$middle = "Joseph "
+$last = "Kanakos "
+
+for ($i = 1; $i -lt 100; $i++) {
+    write-host $first -BackgroundColor Red -ForegroundColor white -NoNewline
+    write-host $middle -BackgroundColor White -ForegroundColor Red -NoNewline
+    write-host $last -BackgroundColor Blue -ForegroundColor Yellow
+}
+
+$number = 28
+
+for ($i = 0; $i -lt 2001; $i++) {
+    $number
+    $number = $number + 3764
+    pause
+}
---
+++ FunStuff/TylerWriteHost.ps1
@@ -0,0 +1,4 @@
+cls
+$name = Read-Host "What is your name?"
+
+Write-Host "Hi, $name! it's awesome to meet you!"
