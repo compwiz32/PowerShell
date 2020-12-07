0377bf0dba0b73a86ba3a6ede71845bb825944b4
Mike Kanakos <mkanakos@gmail.com>
2020-12-06 19:39:35 -0500

    Create PSProfileConfig.ps1

---
+++ PSProfileConfig.ps1
@@ -0,0 +1,85 @@
+function prompt {
+
+    $host.ui.RawUI.WindowTitle = "Current Folder: $pwd"
+
+    $zPromptCurrentFolder = Split-Path -Path $pwd -Leaf
+    $zCmdPromptUser = [Security.Principal.WindowsIdentity]::GetCurrent();
+    $date = Get-Date -Format 'dddd hh:mm:ss tt'
+
+    $zIsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+
+
+    #calc execution time of last cmd and convert to milliseconds, seconds or minutes
+    $lastCommand = Get-History -Count 1
+    if ($lastCommand) { $RunTime = ($lastCommand.EndExecutionTime - $lastCommand.StartExecutionTime).TotalSeconds }
+
+    if ($RunTime -ge 60) {
+        $ts = [timespan]::fromseconds($RunTime)
+        $min, $sec = ($ts.ToString("mm\:ss")).Split(":")
+        $ElapsedTime = -join ($min, " min ", $sec, " sec")
+    }
+    else {
+        $ElapsedTime = [math]::Round(($RunTime), 2)
+        $ElapsedTime = -join (($ElapsedTime.ToString()), " sec")
+    }
+
+
+    #decorate the prompt
+    Write-Host -Object ""
+    Write-host -Object ($(if ($zIsAdmin) { 'Elevated ' } else { '' })) -BackgroundColor DarkRed -ForegroundColor White -NoNewline
+    Write-Host -Object " USER:$($zCmdPromptUser.Name.split("\")[1]) " -BackgroundColor DarkBlue -ForegroundColor White -NoNewline
+    Write-Host -object ".\$zPromptCurrentFolder\ "  -ForegroundColor White -BackgroundColor DarkGray -NoNewline
+    Write-Host -Object " $date " -ForegroundColor White
+    Write-Host "[" -NoNewline -ForegroundColor Green
+    Write-Host $ElapsedTime -ForegroundColor Green -NoNewline
+    Write-host "] " -NoNewline -ForegroundColor Green
+    return "> "
+} #end prompt function
+
+
+function Invoke-5Admin {
+    [CmdletBinding()]
+    param ()
+
+    begin {
+
+    }
+
+    process {
+        Start-Process powershell.exe -Credential aligntech\mkanakos-admin -ArgumentList "Start-Process powershell.exe -Verb runAs"
+    }
+
+    end {
+
+    }
+}
+
+function Invoke-7Admin {
+    [CmdletBinding()]
+    param ()
+
+    begin {
+
+    }
+
+    process {
+        Start-Process powershell.exe -Credential aligntech\mkanakos-admin -ArgumentList "Start-Process pwsh.exe -Verb runAs"
+    }
+
+    end {
+
+    }
+}
+
+#Fav Variables & Shortcuts
+
+$exclude = "runspaceid", "pscomputername"
+
+$InputDir = "C:\Scripts\Input"
+$OutputDir = "C:\Scripts\Output"
+
+$GitIAMDir = "C:\GitRepos\foundational-and-identity"
+$OneDriveDir = "C:\Users\mkanakos\OneDrive - Align Technology, Inc"
+$DownloadsDir = "C:\Users\mkanakos\Downloads"
+$DesktopDir = "C:\Users\mkanakos\Desktop"
+$ReportsDir = "C:\Users\mkanakos\OneDrive - Align Technology, Inc\Reports"
