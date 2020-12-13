b93269c7cd8c5a041cbfbdade51b9c8c580b33e3
compwiz32 <mkanakos@gmail.com>
2020-12-12 21:49:45 -0500

    move file to new folder

--- Profile/PSProfileConfig.ps1
+++ Profile/PSProfileConfig.ps1
@@ -1,16 +1,18 @@
 function prompt {
 
+    #Assign Windows Title Text
     $host.ui.RawUI.WindowTitle = "Current Folder: $pwd"
 
-    $zPromptCurrentFolder = Split-Path -Path $pwd -Leaf
-    $zCmdPromptUser = [Security.Principal.WindowsIdentity]::GetCurrent();
-    $date = Get-Date -Format 'dddd hh:mm:ss tt'
+    #Configure current user, current folder and date outputs
+    $CmdPromptCurrentFolder = Split-Path -Path $pwd -Leaf
+    $CmdPromptUser = [Security.Principal.WindowsIdentity]::GetCurrent();
+    $Date = Get-Date -Format 'dddd hh:mm:ss tt'
 
-    $zIsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+    # Test for Admin / Elevated
+    $IsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 
-
-    #calc execution time of last cmd and convert to milliseconds, seconds or minutes
-    $lastCommand = Get-History -Count 1
+    #Calculate execution time of last cmd and convert to milliseconds, seconds or minutes
+    $LastCommand = Get-History -Count 1
     if ($lastCommand) { $RunTime = ($lastCommand.EndExecutionTime - $lastCommand.StartExecutionTime).TotalSeconds }
 
     if ($RunTime -ge 60) {
@@ -23,13 +25,12 @@ function prompt {
         $ElapsedTime = -join (($ElapsedTime.ToString()), " sec")
     }
 
-
-    #decorate the prompt
-    Write-Host -Object ""
-    Write-host -Object ($(if ($zIsAdmin) { 'Elevated ' } else { '' })) -BackgroundColor DarkRed -ForegroundColor White -NoNewline
-    Write-Host -Object " USER:$($zCmdPromptUser.Name.split("\")[1]) " -BackgroundColor DarkBlue -ForegroundColor White -NoNewline
-    Write-Host -object ".\$zPromptCurrentFolder\ "  -ForegroundColor White -BackgroundColor DarkGray -NoNewline
-    Write-Host -Object " $date " -ForegroundColor White
+    #Decorate the CMD Prompt
+    Write-Host ""
+    Write-host ($(if ($IsAdmin) { 'Elevated ' } else { '' })) -BackgroundColor DarkRed -ForegroundColor White -NoNewline
+    Write-Host " USER:$($CmdPromptUser.Name.split("\")[1]) " -BackgroundColor DarkBlue -ForegroundColor White -NoNewline
+    Write-Host ".\$CmdPromptCurrentFolder\ "  -ForegroundColor White -BackgroundColor DarkGray -NoNewline
+    Write-Host " $date " -ForegroundColor White
     Write-Host "[" -NoNewline -ForegroundColor Green
     Write-Host $ElapsedTime -ForegroundColor Green -NoNewline
     Write-host "] " -NoNewline -ForegroundColor Green
