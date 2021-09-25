dff684266013849864810baadef202c03ff2556a
compwiz32 <mkanakos@gmail.com>
2021-09-25 14:49:48 -0400

    misc code captures

--- !Scratchpad.ps1
+++ !Scratchpad.ps1
@@ -24,3 +24,25 @@ Get-ChildItem C:\Scripts -Recurse | ForEach-Object {
     $array.Path = $Path
     $array.PathLength = $PathLength
 }
+
+
+
+
+
+$LastCommand = Get-History -Count 1
+    if ($lastCommand) { $RunTime = ($lastCommand.EndExecutionTime - $lastCommand.StartExecutionTime).TotalSeconds }
+
+    if ($RunTime -ge 60) {
+        $ts = [timespan]::fromseconds($RunTime)
+        $min, $sec = ($ts.ToString("mm\:ss")).Split(":")
+        $ElapsedTime = -join ($min, " min ", $sec, " sec")
+    }
+    else {
+        $ElapsedTime = [math]::Round(($RunTime), 2)
+        $ElapsedTime = -join (($ElapsedTime.ToString()), " sec")
+    }
+
+
+
+
+
